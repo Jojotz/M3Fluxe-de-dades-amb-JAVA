db93cbc57298cc882f57ba25479cd8c57404d9df
Joan Coll <67835708+Jojotz@users.noreply.github.com>
2020-09-21 12:14:57 +0200

    Add files via upload

--- Fase4M3.java
+++ Fase4M3.java
@@ -5,7 +5,7 @@ public class Fase4M3 {
 	
 	public static void main (String[] args) { 
 		
-		Scanner entrada=new Scanner(System.in);
+		Scanner entrada=new Scanner(System.in);				// Cogemos input de ciudades
 		
 		System.out.println("Introdueix el nom d'una ciutat: ");
 		String city1=entrada.nextLine();
@@ -27,8 +27,7 @@ public class Fase4M3 {
 		
 		System.out.println("Ciutats introudïdes: "+city1+","+city2+","+city3+","+city4+","+city5+","+city6);
 	
-		//char [] arraycity1 = city1.toCharArray(); ... Se podría haber seguido esta via.
-				
+		//Declaro array por cada ciudad:
 		String [] arrayCity1 = new String [city1.length()];
 		String [] arrayCity2 = new String [city2.length()];
 		String [] arrayCity3 = new String [city3.length()];
@@ -36,6 +35,7 @@ public class Fase4M3 {
 		String [] arrayCity5 = new String [city5.length()];
 		String [] arrayCity6 = new String [city6.length()];
 		
+		//Paso cada array por función para rellenarlo letra por letra:
 		fillArray (arrayCity1);
 		fillArray (arrayCity2);
 		fillArray (arrayCity3);
@@ -43,6 +43,7 @@ public class Fase4M3 {
 		fillArray (arrayCity5);
 		fillArray (arrayCity6);
 		
+		//Paso cada array por función para invertirlos:
 		arrangeArray (arrayCity1);
 		arrangeArray (arrayCity2);
 		arrangeArray (arrayCity3);
@@ -50,6 +51,7 @@ public class Fase4M3 {
 		arrangeArray (arrayCity5);
 		arrangeArray (arrayCity6);
 	
+		//Paso arrays invertidos por función para mostrar por pantalla:
 		printArray (arrayCity1);
 		printArray (arrayCity2);
 		printArray (arrayCity3);
@@ -75,12 +77,13 @@ public class Fase4M3 {
 		    list.toArray(arrayFun);
 		 }
 		
-		public static void printArray(String [] arrayFun) {
+		public static void printArray(String [] arrayFun) { //Construyo string desde string Array y printo
 			
-			for (int i = 0; i <arrayFun.length - 1; i++) {         
-				System.out.print(arrayFun[i] + ",");
-				}
-	        System.out.print(arrayFun[arrayFun.length - 1] + "  ");
-		
+			StringBuilder builder = new StringBuilder();
+			for (String value : arrayFun) {
+			    builder.append(value);
+			}
+			String city = builder.toString();
+			System.out.println(city);
 		}
 }		
