46777b8e0421f003a3a0a2d903adef00acec62c4
Joan Coll <67835708+Jojotz@users.noreply.github.com>
2020-09-23 09:56:58 +0200

    Add files via upload

--- Fase4M3.java
+++ Fase4M3.java
@@ -1,11 +1,11 @@
-/*Joan Coll JAVA 2  16/09/2020 */
+/*Joan Coll JAVA 2  22/09/2020 */
 import java.util.*;
 
 public class Fase4M3 {
 	
 	public static void main (String[] args) { 
 		
-		Scanner entrada=new Scanner(System.in);				// Cogemos input de ciudades
+		Scanner entrada=new Scanner(System.in);				// Cojo input de ciudades
 		
 		System.out.println("Introdueix el nom d'una ciutat: ");
 		String city1=entrada.nextLine();
@@ -27,63 +27,45 @@ public class Fase4M3 {
 		
 		System.out.println("Ciutats introudïdes: "+city1+","+city2+","+city3+","+city4+","+city5+","+city6);
 	
-		//Declaro array por cada ciudad:
-		String [] arrayCity1 = new String [city1.length()];
-		String [] arrayCity2 = new String [city2.length()];
-		String [] arrayCity3 = new String [city3.length()];
-		String [] arrayCity4 = new String [city4.length()];
-		String [] arrayCity5 = new String [city5.length()];
-		String [] arrayCity6 = new String [city6.length()];
+		//Paso a char array por cada string ciudad:
 		
-		//Paso cada array por función para rellenarlo letra por letra:
-		fillArray (arrayCity1);
-		fillArray (arrayCity2);
-		fillArray (arrayCity3);
-		fillArray (arrayCity4);
-		fillArray (arrayCity5);
-		fillArray (arrayCity6);
+		char[] charCity1= city1.toCharArray();
+		char[] charCity2= city2.toCharArray();
+		char[] charCity3= city3.toCharArray();
+		char[] charCity4= city4.toCharArray();
+		char[] charCity5= city5.toCharArray();
+		char[] charCity6= city6.toCharArray();
 		
-		//Paso cada array por función para invertirlos:
-		arrangeArray (arrayCity1);
-		arrangeArray (arrayCity2);
-		arrangeArray (arrayCity3);
-		arrangeArray (arrayCity4);
-		arrangeArray (arrayCity5);
-		arrangeArray (arrayCity6);
+		//Paso cada array de chars por función para invertirlos y asigno a nuevos Arrays reversed:
+		
+		char[] reversedCity1 = arrangeArray (charCity1);
+		char[] reversedCity2 = arrangeArray (charCity2);
+		char[] reversedCity3 = arrangeArray (charCity3);
+		char[] reversedCity4 = arrangeArray (charCity4);
+		char[] reversedCity5 = arrangeArray (charCity5);
+		char[] reversedCity6 = arrangeArray (charCity6);
 	
 		//Paso arrays invertidos por función para mostrar por pantalla:
-		printArray (arrayCity1);
-		printArray (arrayCity2);
-		printArray (arrayCity3);
-		printArray (arrayCity4);
-		printArray (arrayCity5);
-		printArray (arrayCity6);
-	}
-				
-		public static void fillArray(String [] arrayFun) {
 		
-			for (int i = 0; i <arrayFun.length; i++) {							
-				
-				Scanner entrada2=new Scanner(System.in);
-				System.out.println("Introdueix una lletra: ");
-				arrayFun [i]= entrada2.nextLine();			
-			}		
-		}
+		printArray (reversedCity1);
+		printArray (reversedCity2);
+		printArray (reversedCity3);
+		printArray (reversedCity4);
+		printArray (reversedCity5);
+		printArray (reversedCity6);
+	}
+						
+		public static char[] arrangeArray(char[] arrayFun) {
 			
-		public static void arrangeArray(String [] arrayFun) {
-			
-			List<Object> list = Arrays.asList(arrayFun);
-		    Collections.reverse(list);
-		    list.toArray(arrayFun);
+			char arrayRev[] = new char[arrayFun.length];
+		    for (int i = arrayFun.length - 1; i >= 0; i--) {
+		    	arrayRev[i] = arrayFun[(arrayFun.length - 1) - i];
+		    }
+		    return arrayRev;
 		 }
 		
-		public static void printArray(String [] arrayFun) { //Construyo string desde string Array y printo
+		public static void printArray(char [] arrayFun) { 
 			
-			StringBuilder builder = new StringBuilder();
-			for (String value : arrayFun) {
-			    builder.append(value);
-			}
-			String city = builder.toString();
-			System.out.println(city);
+			System.out.println(arrayFun);
 		}
-}		
+}	
